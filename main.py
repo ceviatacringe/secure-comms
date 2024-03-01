0ccd1ba648a6544e6fb0220e77b623141c28ef6a
Alex <revellxd@gmail.com>
2024-03-02 01:44:13 +0200

    Updated menu

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import os
 from menufunctions import *
 from encryption import generate_aes_key
 
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
+# Delay time (seconds) between virtual keyboard usage 
+# The lower the faster, too low and it might break depending on your specs.
+DELAY = 0.005
+
 
 def main():
     encrypt_hotkey = 'insert'
@@ -14,27 +15,33 @@ def main():
         print(f"Keyword: {keyword}")
         print(f"Encrypt hotkey: {encrypt_hotkey}")
         print(f"Decrypt hotkey: {decrypt_hotkey}\n")
-        print("1. Change keyword")
-        print("2. Change encryption hotkey")
-        print("3. Change decryption hotkey")
-        print("4. Instructions")
-        print("5. Exit\n")
+        print("1. Start")
+        print("2. Change keyword")
+        print("3. Change encryption hotkey")
+        print("4. Change decryption hotkey")
+        print("5. Instructions\n")
+        print("6. Exit\n")
 
         choice = input()
 
         if choice == '1':
             clear()
-            keyword = input("Update keyword: ")
+            print(f"Encrypt: {encrypt_hotkey}")
+            print(f"Decrypt: {decrypt_hotkey}")
+            runner(encrypt_hotkey, decrypt_hotkey, keyword, DELAY)
         elif choice == '2':
             clear()
-            encrypt_hotkey = get_hotkey()
+            keyword = input("Update keyword: ")
         elif choice == '3':
             clear()
-            decrypt_hotkey = get_hotkey()
+            encrypt_hotkey = get_hotkey()
         elif choice == '4':
             clear()
-            instructions()
+            decrypt_hotkey = get_hotkey()
         elif choice == '5':
+            clear()
+            instructions()
+        elif choice == '6':
             clear()
             print("Exiting.")
             break
